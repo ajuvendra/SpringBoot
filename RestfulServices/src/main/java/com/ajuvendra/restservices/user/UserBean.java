f27a7174381bbea5d650213ba319e49dddbe042e
Ajuvendra Kumar <ajuvendra@gmail.com>
2019-04-21 15:46:42 +0530

    Added Filtering both static and Dynamic

--- RestfulServices/src/main/java/com/ajuvendra/restservices/user/UserBean.java
+++ RestfulServices/src/main/java/com/ajuvendra/restservices/user/UserBean.java
@@ -5,12 +5,18 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "All user details")
 public class UserBean {
 	private Integer id;
 	@Size(min = 2, message = "Name must be 2 or more characters")
+	@ApiModelProperty(notes = "Name can not be less than 2 characters")
 	private String name;
 	
 	@Past(message = "Birthdate can not be in past")
+	@ApiModelProperty(notes = "Birth date should be in past")
 	private Date birthDate;
 
 	public UserBean(Integer id, String name, Date birthDate) {
