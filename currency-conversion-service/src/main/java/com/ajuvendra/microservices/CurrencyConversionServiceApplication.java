6253642de709628c87388483a2026ef0432f078c
Ajuvendra Kumar <ajuvendra@gmail.com>
2019-05-25 12:13:23 +0530

    Currency Conversion Service Project

---
+++ currency-conversion-service/src/main/java/com/ajuvendra/microservices/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ajuvendra.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.ajuvendra.microservices")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
